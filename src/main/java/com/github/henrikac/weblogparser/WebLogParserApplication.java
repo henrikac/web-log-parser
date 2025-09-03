c3c39849db28008defbc87b85f6bf8d5b604df93
Henrik Christensen <sensen1695@hotmail.com>
2025-09-03 17:31:59 +0200

    initial commit

---
+++ src/main/java/com/github/henrikac/weblogparser/WebLogParserApplication.java
@@ -0,0 +1,13 @@
+package com.github.henrikac.weblogparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLogParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/henrikac/weblogparser/WebLogParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.henrikac.weblogparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLogParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
